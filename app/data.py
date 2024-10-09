46be6f2aa696295571e9d049bec472b07119856f
Wayne Bishop <waynewbishop@users.noreply.github.com>
2024-10-08 19:10:48 -0700

    Starting work on OpenWeatherAPI..

--- app/data.py
+++ app/data.py
@@ -2,7 +2,8 @@
 import urllib.request
 import json
 
-from datetime import datetime
+from typing import List, Dict, Any
+
 
 class Data:
     """
@@ -18,8 +19,6 @@ class Data:
         (List important methods here, e.g., data collection, cleaning, and storage methods)
     """
 
-    _api_key = None
-
     def __init__(self):
         """
         Initialize the Data instance.
@@ -28,19 +27,67 @@ class Data:
         any required data structures or storage mechanisms.
         """
 
-        # TODO: initialize the config.json access here
+        # initialization of JSON dictionary
+        self._config: List[Dict[str, Any]] = []
 
-        pass
+
+    def load_config(self):
+        """
+        Attempts to load JSON data.
+
+        Provides configuration options for entire recommender service
+        :return:
+        """
+
+        try:
+            with open("../config.json", "r") as datafile:
+                self._config = json.load(datafile)
+                print("json data loaded successfully.")
+                return
+
+        except FileNotFoundError:
+            print("Error: The file '30DayQuakes.json' was not found.")
+
+        except json.JSONDecodeError:
+            print("Error: The file contains invalid JSON.")
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
 
-    @staticmethod
-    def get_weather_by_coords(self, lat, lon, api_key, units):
 
-        base_url = "http://api.openweathermap.org/data/2.5/weather"
+    def get_weather(self, lat, lon):
+        """
+        Obtains the weather forecast for the current date
+        and location.
+        :param lat: Latitude
+        :param lon: Longitude
+        :return:
+        """
+
+        # check for an empty list
+        if not self._config:
+            self.load_config()
+
+        # set the json api key
+        api_key = self._config['weather_api']['api_key']
+
+        # set the default weather units
+        units = self._config['weather_api']['units']
+
+        # set the url endpoint
+        base_url = self._config['weather_api']['endpoint']
+
+        # set the full url with querystring
         url = f"{base_url}?lat={lat}&lon={lon}&appid={api_key}&units={units}"
 
+        # TODO: Add try catch statement here as well as logging
+
+        # initiate the connection
         with urllib.request.urlopen(url) as response:
             data = response.read().decode()
 
         weather_data = json.loads(data)
         return weather_data
+
+        # TODO: Filter down data to match the current date
