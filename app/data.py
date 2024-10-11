cccfad5a5fc1b957ffd73606ab2bc3195c11827b
Wayne Bishop <waynewbishop@users.noreply.github.com>
2024-10-10 21:46:46 -0700

    Continued work building out the weather model..

--- app/data.py
+++ app/data.py
@@ -1,8 +1,6 @@
+from typing import List
 
-import urllib.request
-import json
-
-from typing import List, Dict, Any
+from app.weather import Weather
 
 
 class Data:
@@ -27,67 +25,19 @@ class Data:
         any required data structures or storage mechanisms.
         """
 
-        # initialization of JSON dictionary
-        self._config: List[Dict[str, Any]] = []
+        # TODO: instead of this being a list it should be a strongly typed object.
 
+        self._vector = List[float]
 
-    def load_config(self):
+    @staticmethod
+    def prepare_vector(lat, long):  #37.7749, -122.4194
         """
-        Attempts to load JSON data.
+        Prepares model vector for model evaluation.
 
-        Provides configuration options for entire recommender service
         :return:
         """
 
-        try:
-            with open("../config.json", "r") as datafile:
-                self._config = json.load(datafile)
-                print("json data loaded successfully.")
-                return
+        # new weather instance
+        w = Weather()
+        w.get_weather(lat, long)
 
-        except FileNotFoundError:
-            print("Error: The file '30DayQuakes.json' was not found.")
-
-        except json.JSONDecodeError:
-            print("Error: The file contains invalid JSON.")
-
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-
-
-
-    def get_weather(self, lat, lon):
-        """
-        Obtains the weather forecast for the current date
-        and location.
-        :param lat: Latitude
-        :param lon: Longitude
-        :return:
-        """
-
-        # check for an empty list
-        if not self._config:
-            self.load_config()
-
-        # set the json api key
-        api_key = self._config['weather_api']['api_key']
-
-        # set the default weather units
-        units = self._config['weather_api']['units']
-
-        # set the url endpoint
-        base_url = self._config['weather_api']['endpoint']
-
-        # set the full url with querystring
-        url = f"{base_url}?lat={lat}&lon={lon}&appid={api_key}&units={units}"
-
-        # TODO: Add try catch statement here as well as logging
-
-        # initiate the connection
-        with urllib.request.urlopen(url) as response:
-            data = response.read().decode()
-
-        weather_data = json.loads(data)
-        return weather_data
-
-        # TODO: Filter down data to match the current date
--- app/weather.py
+++ app/weather.py
@@ -1,5 +1,117 @@
 
-class Weather:
-    def __init__(self):
-        pass
+import urllib.request
+import urllib.error
+import json
 
+from datetime import datetime, timezone
+from typing import List, Dict, Any
+
+
+class Weather:
+    """
+
+    """
+    def __init__(self):
+        """
+        Provides class initialization
+        """
+
+        # initialization of JSON dictionary
+        self._config: List[Dict[str, Any]] = []
+
+
+    def load_config(self):
+        """
+        Attempts to load JSON data.
+
+        Provides configuration options for entire recommender service
+        :return:
+        """
+
+        # check for instance
+        if not self._config:
+            return
+
+        try:
+            with open("../config.json", "r") as datafile:
+                self._config = json.load(datafile)
+                print("json data loaded successfully.")
+                return
+
+        except FileNotFoundError:
+            print("Error: The file '30DayQuakes.json' was not found.")
+
+        except json.JSONDecodeError:
+            print("Error: The file contains invalid JSON.")
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+
+    def get_weather(self, lat: float, lon: float):
+        """
+        Obtains the weather forecast for the current date
+        and location.
+        :param lat: Latitude
+        :param lon: Longitude
+        :return: A tuple containing the temperature,
+        """
+
+        # container for resulting weather data
+        weather_data: List[Dict[str, Any]] = None
+
+        # check for empty list
+        if not self._config:
+            self.load_config()
+
+        # set the json api key
+        api_key = self._config['weather_api']['api_key']
+
+        # set the default weather units
+        units = self._config['weather_api']['units']
+
+        # set the url endpoint
+        base_url = self._config['weather_api']['endpoint']
+
+        # set the full url with querystring
+        url = f"{base_url}?lat={lat}&lon={lon}&appid={api_key}&units={units}"
+
+        try:
+            # initiate the connection
+            with urllib.request.urlopen(url) as response:
+                data = response.read().decode()
+
+            weather_data = json.loads(data)
+
+        except urllib.error.URLError as e:
+            print(f"Error accessing URL: {e}")  # replace with log statement
+
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")  # replace with log statement
+
+        # obtain forecasts
+        forecasts = weather_data['list']
+
+        # Find the closest matching forecast
+        closest = self._filter_forecast(forecasts)
+
+        # Convert the timestamp to a datetime object for readability
+        forecast_current = datetime.fromtimestamp(closest['dt'], tz=timezone.utc)
+
+        # TODO: Build out a condensed JSON model containing only the info I need.
+
+        # review final results
+        print(f"closest forecast time is {forecast_current}: \n{closest}")
+
+
+    @staticmethod
+    def _filter_forecast(forecasts) -> List[Dict[str, Any]]:
+        """
+        Helper method that iterates through the
+        daily forecasts and find the closest match to the current datetime.
+        :param forecasts:
+        :return:
+        """
+        current_timestamp = datetime.now(timezone.utc).timestamp()
+
+        return min(forecasts, key=lambda forecast: abs(forecast['dt'] - current_timestamp))
